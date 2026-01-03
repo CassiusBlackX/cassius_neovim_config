3e410e8d48ff932bf9a929a62b3d356242cba975
CassiusBlackX <cassiusx@qq.com>
2026-01-03 17:31:28 +0800

    update core/options, safe zone when scrolling

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,9 +6,13 @@ opt.cursorline = false
 opt.termguicolors = true
 opt.timeoutlen = 300
 opt.updatetime = 300
+-- safe zone
+opt.scrolloff = 5
+opt.sidescrolloff = 8
+
+-- fold
 opt.foldmethod = "expr"
 opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
 opt.foldlevel = 99
 opt.foldcolumn = "0"
--- vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
